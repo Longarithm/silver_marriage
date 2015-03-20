91ad5628e61a89999f5dbca0d07ba7c3d37febd8
Fata1ist <fata1ist@inbox.ru>
2015-03-20 19:16:20 +0300

    BSUIR

--- structures/segment_tree_Anton.cpp
+++ structures/segment_tree_Anton.cpp
@@ -1,5 +1,8 @@
+const int MAXN = 500500;
+const int MAXK = 262144;
+    
 struct segment_tree {
-    ll value, number[2];
+    ll value, value2, number[2];
     bool colour[2];
 };
 
@@ -8,6 +11,7 @@ segment_tree t[MAXK * 2 + 1];
 void tree_build() {
 	forn(i, MAXK * 2 + 1) {
         t[i].value = 0;
+		t[i].value2 = 0;
         forn(j, 2) {
 			t[i].colour[j] = 0;
 			t[i].number[j] = 0;
@@ -24,11 +28,15 @@ void push(int v, int l, int r) {
 				if (i == 0) {
 					t[2 * v].number[i] += t[v].number[i];
 					t[2 * v + 1].number[i] += t[v].number[i];
+					t[2 * v].value2 += t[2 * v].value * t[v].number[i] * 2 + t[v].number[i] * t[v].number[i] * len;
+					t[2 * v + 1].value2 += t[2 * v + 1].value * t[v].number[i] * 2 + t[v].number[i] * t[v].number[i] * len;
 					t[2 * v].value += t[v].number[i] * len;
 					t[2 * v + 1].value += t[v].number[i] * len;
 				} else {
 					t[2 * v].number[i] = t[v].number[i];
 					t[2 * v + 1].number[i] = t[v].number[i];
+					t[2 * v].value2 = t[v].number[i] * t[v].number[i] * len;
+					t[2 * v + 1].value2 = t[v].number[i] * t[v].number[i] * len;
 					t[2 * v].value = t[v].number[i] * len;
 					t[2 * v + 1].value = t[v].number[i] * len;
 				}
@@ -39,7 +47,7 @@ void push(int v, int l, int r) {
 	}
 }
 
-void update(int v, int l, int r, int a, int b, int x, int type) {
+void update(int v, int l, int r, int a, int b, ll x, int type) {
     push(v, l, r);
     if ((r < a) || (b < l))
         return;
@@ -47,9 +55,11 @@ void update(int v, int l, int r, int a, int b, int x, int type) {
         t[v].colour[type] = 1;
 		if (type == 0) {
 			t[v].number[type] += x;
+			t[v].value2 += t[v].value * x * 2 + x * x * (r - l + 1);
 			t[v].value += x * (r - l + 1);
 		} else {
 			t[v].number[type] = x;
+			t[v].value2 = x * x * (r - l + 1);
 			t[v].value = x * (r - l + 1);
 		}		
         return;
@@ -60,11 +70,14 @@ void update(int v, int l, int r, int a, int b, int x, int type) {
     t[v].value = t[2 * v].value + t[2 * v + 1].value;
 }
 
-ll get(int v, int l, int r, int a, int b) {
+ll get(int v, int l, int r, int a, int b, int type) {
     push(v, l, r);
     if ((r < a) || (b < l))
         return 0;
-    if ((a <= l) && (r <= b))
-        return t[v].value;
-    return get(2 * v, l, (l + r) / 2, a, b) + get(2 * v + 1, (l + r) / 2 + 1, r, a, b);
+    if ((a <= l) && (r <= b)) {
+		if (type == 0)
+			return t[v].value;
+		return t[v].value2;
+	}
+    return get(2 * v, l, (l + r) / 2, a, b, type) + get(2 * v + 1, (l + r) / 2 + 1, r, a, b, type);
 }
